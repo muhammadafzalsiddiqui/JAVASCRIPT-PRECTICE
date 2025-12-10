e256fdd8c657b809658e738c0d0a5b33797908b0
muhammadafzalsiddiqui <khanafzal155@gmail.com>
2025-12-10 18:56:24 +0500

    variable fo string prectice

--- java.js
+++ java.js
@@ -1,5 +1,16 @@
-alert("Thanks for your input!");
-console.log()
-window.log(6+7)
+// alert("Thanks for your input!");
+// console.log()
+// window.log(6+7)
 
+// Practice Questions on String Variable
+
+
+// var name = "afzal"
+// alert(name)
+ 
+// var city = "karachi"
+// alert("i love"  + city)
+// var  name;
+// name = "afzal"
+// alert(name)
 
