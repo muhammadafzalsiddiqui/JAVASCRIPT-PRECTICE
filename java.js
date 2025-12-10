820f455782e3932881f5b9d8c44a01897b72972e
muhammadafzalsiddiqui <khanafzal155@gmail.com>
2025-12-10 20:50:35 +0500

    if else if prectice

--- java.js
+++ java.js
@@ -31,4 +31,103 @@
 //  var num1 = 4
 //  alert(num1 +4)
 
-var
+// Math expressions:Familiar operators
+
+//  var num = 2
+//  alert(num + 3)
+
+//  we can also write 
+// alert( 2 + 5)
+
+// alert ( 10 % 2)
+
+// var num = 1
+// var num2 = num++
+// alert(num2)
+//  var a = 1
+//  var b = a++ + a++
+//  console.log(b)
+
+//  var a = 1
+//  var c = 2
+//  var b = a++ + a++ + c++ - --a + a++ + c++
+//  console.log(b)
+
+//  var a = 2
+//  var b = 1
+//  var c = 3
+//  var d = 4
+//  var e = a++ - b++ + a++ + c++ + ++d + a++ + c++ + ++d
+//  console.log(e)
+// var a = 1
+// console.log(--a)
+
+// console.log(3/ 5);
+
+// var firstName = "afzal"
+// console.log(firstName+" siddiqui")
+
+// var a = prompt("How many cats do you have")
+
+// console.log("i have seven cats " + a)
+// console.log(a);
+// let a = 10;
+//  out put Error  becouse js work top to botttom
+
+// let a = 10;
+// onsole.log(a);
+
+// function test(x = y, y = 2) {
+//   return x + y;
+// }
+// console.log(test());
+
+
+// var marks = prompt("Enter your percetage");
+// if( marks >= 90){
+//     console.log("Grade A1")
+// }
+// else if (marks >= 80){
+//     console.log("Grade A")
+// }
+// else if(marks >= 60){
+//     console.log("grade B")
+// }
+// else
+//     (marks<=59)
+//     console.log("fail")
+    
+// var correctAnswer = "Vatican";
+// if (x === correctAnswer) {
+//   alert("Correct!");
+// }
+// ReferenceError
+
+// let firstName = "Ali";
+// let lastName = "Raza";
+// let fullName = "Ali Raza";
+
+// if (fullName === firstName + " " + lastName) {
+//   console.log("Correct!");
+// } else {
+//   console.log("Wrong!");
+// }
+
+// 1 if (weight > 300 &&
+// time < 6) {
+// 2   alert("Come to our tryout!");
+// 3 }
+// 4 else {
+// 5   alert("Come to our cookout!");
+// 6 }
+//  var color = prompt("Enter A color")
+//  if ( color === "red"){
+//     console.log("stop")
+//  }
+//  else if(color === "green"){
+//     console.log("go")
+//  }
+//  else(
+//     console.log("color not match")
+//  )
+
