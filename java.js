f1492b44f8c0cb5add706118e3a32a9f4e8beaf4
muhammadafzalsiddiqui <khanafzal155@gmail.com>
2025-12-10 19:05:28 +0500

    variable for number prectice

--- java.js
+++ java.js
@@ -10,7 +10,25 @@
  
 // var city = "karachi"
 // alert("i love"  + city)
+
 // var  name;
 // name = "afzal"
 // alert(name)
 
+// var name = "ali"
+// var name = "ali-1"
+// alert(name)
+
+// Variables for Numbers
+
+// var num = 2
+// alert(num)
+
+// var num1 = 3
+// var num2 = 4
+// alert(num1 + num2)
+
+//  var num1 = 4
+//  alert(num1 +4)
+
+var
